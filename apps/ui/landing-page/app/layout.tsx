8bc613c4e949253d221c04030806cde4edf10b1d
jivaros <jivarosgbeto@gmail.com>
2025-02-19 15:25:42 +0100

    chore(ui): app docs

--- apps/ui/landing-page/app/layout.tsx
+++ apps/ui/landing-page/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Logengine',
+  description: 'Powerful Log Management'
 }
 
 export default function RootLayout({
